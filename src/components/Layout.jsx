d726743b38c654e9b7036fa76e5e82249ffe22dc
Josen Romero <romerojosemanuel@hotmail.com>
2022-12-12 20:34:45 -0600

    update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,15 @@
 const Layout = ({children}) => {
 
   return (
-    <>
+    <div className="min-h-screen grid grid-cols-1 place-content-between">
       <p>Navbar</p>
-      {children}
+      <main className="md:w-3/4 mx-auto">
+        <div className="p-6 m-2 bg-white rounded-xl text-zinc-800">
+          {children}
+        </div>
+      </main>
       <p>Footer</p>
-    </>
+    </div>
   )
 
 }
