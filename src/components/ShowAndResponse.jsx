28c595debd3a681cb48d298a0a16c799f4879e06
Josen Romero <romerojosemanuel@hotmail.com>
2022-12-15 19:56:42 -0600

    refactor PlayPage file

---
+++ src/components/ShowAndResponse.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useWords } from '../hooks/useWords'
+import Form from '../components/Form'
+import { checkingResponse } from '../utils/utils'
+
+const ShowAndResponse = () => {
+
+  const { words, word, showingWords } = useWords();
+  const [response, setResponse] = useState({});
+
+  const handleResponse = (event) => {
+    event.preventDefault();
+    let { incorrectWord, win } = checkingResponse(words, response);
+    console.log(incorrectWord, win)
+  }
+
+  const handleInput = ({target: {name, value}}) => {
+    setResponse({ ...response, [name] : value })
+  }
+
+  return (
+    <div className="text-center">
+      {showingWords && 
+        <div>{word}</div>
+      }
+      {!showingWords && 
+        <Form 
+          handleResponse={handleResponse}
+          handleInput={handleInput}
+          arr={[...Array(words.length - 1)]}
+        />
+      }
+    </div>
+  )
+
+}
+
+export default ShowAndResponse
--- src/pages/PlayPage.jsx
+++ src/pages/PlayPage.jsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
-import { useWords } from '../hooks/useWords'
-import Form from '../components/Form'
-import { checkingResponse } from '../utils/utils'
+import ShowAndResponse from '../components/ShowAndResponse'
 
 const PlayPage = () => {
-  
-  const { words, word, showingWords } = useWords();
-  const [response, setResponse] = useState({});
-
-  const handleResponse = (event) => {
-    event.preventDefault();
-    let { incorrectWord, win } = checkingResponse(words, response);
-    console.log(incorrectWord, win)
-  }
-
-  const handleInput = ({target: {name, value}}) => {
-    setResponse({ ...response, [name] : value })
-  }
 
   return (
-    <div className="text-center">
-      {showingWords && 
-        <div>{word}</div>
-      }
-      {!showingWords && 
-        <Form 
-          handleResponse={handleResponse}
-          handleInput={handleInput}
-          arr={[...Array(words.length - 1)]}
-        />
-      }
-    </div>
+    <>
+      <ShowAndResponse />
+    </>
   )
 
 }
