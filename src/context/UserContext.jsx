b410b8b36107d9f029062c93a4ec9a61d661f3a8
Josen Romero <romerojosemanuel@hotmail.com>
2022-12-21 18:19:42 -0600

    feat: save data functionality

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -10,24 +10,39 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
 
+    checkingCurrentData();
+
+  }, [])
+
+  const checkingCurrentData = () => {
     let currentLevel = localStorage.getItem('level');
+    let currentNumberOfWords = localStorage.getItem('numberOfWords');
 
     if(currentLevel === null) {
       localStorage.setItem('level', JSON.stringify(INITIAL_LEVEL));
+      localStorage.setItem('numberOfWords', JSON.stringify(INITIAL_NUMBER_OF_WORDS));
       currentLevel = INITIAL_LEVEL;
+      currentNumberOfWords = INITIAL_NUMBER_OF_WORDS;
     }
 
     setLevel(JSON.parse(currentLevel));
+    setNumberOfWords(JSON.parse(currentNumberOfWords));
 
-  }, [])
+  }
 
   const nextLevel = () => {
 
     if(level < TOTAL_LEVELS) {
+      
       let newValue = level + 1;
       localStorage.setItem('level', JSON.stringify(newValue));
       setLevel(newValue);
-      if(newValue % 3 === 0 && newValue !== 9 && newValue !== 15) setNumberOfWords(numberOfWords + 1);
+
+      if(newValue % 3 === 0 && newValue !== 9 && newValue !== 15) {
+        localStorage.setItem('numberOfWords', JSON.stringify(numberOfWords + 1));
+        setNumberOfWords(numberOfWords + 1);
+      }
+
     }
 
   }
