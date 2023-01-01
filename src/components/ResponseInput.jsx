a09e22ded6682d08d75550af27021eab095593c2
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-01 16:52:10 -0600

    fix empty response

--- src/components/ResponseInput.jsx
+++ src/components/ResponseInput.jsx
@@ -6,30 +6,38 @@ const ResponseInput = ({ handleInput }) => {
   const ref = useRef(null);
 
   const nextResponse = () => {
-    ref.current.focus();
     handleInput(currentResponse);
     setCurrentResponse("");
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    nextResponse();
+    ref.current.focus();
+  }
+
   const onKeyDownHandler = (e) => {
-    if(e.keyCode === 13) nextResponse();
+    if(e.keyCode === 13 && currentResponse !== "") nextResponse();
   }
 
   return (
     <div className="mt-4">
       <div className="mb-4">Escribe las palabras en el orden correcto</div>
       <div className="mb-4">
-        <input
-          type="text"
-          name="response"
-          ref={ref}
-          onKeyDown={onKeyDownHandler}
-          onChange={(e) => setCurrentResponse(e.target.value)}
-          value={currentResponse}
-          placeholder={`Agregar la palabra`}
-          className="bg-white border text-gray-700 rounded py-2 px-3 shadow"
-        />
-        <button onClick={() => nextResponse()} className="ml-3 mt-3 bg-sky-500 hover:bg-sky-700 px-5 py-2 font-semibold text-white">Add</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="response"
+            ref={ref}
+            onKeyDown={onKeyDownHandler}
+            onChange={(e) => setCurrentResponse(e.target.value)}
+            value={currentResponse}
+            placeholder={`Agregar la palabra`}
+            className="bg-white border text-gray-700 rounded py-2 px-3 shadow"
+            required
+          />
+          <button className="ml-3 mt-3 bg-sky-500 hover:bg-sky-700 px-5 py-2 font-semibold text-white">Add</button>
+        </form>
       </div>
     </div>
   )
