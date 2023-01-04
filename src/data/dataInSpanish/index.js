5f5b7c85ce84dad9b90f22e717fd7e7163acfa01
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-03 19:14:25 -0600

    add bodyParts data

--- src/data/dataInSpanish/index.js
+++ src/data/dataInSpanish/index.js
@@ -1,7 +1,9 @@
 import { animals } from './animals.js'
+import { bodyParts } from './bodyParts.js'
 import { foods } from './foods.js'
 
 export const data_in_spanish = [
   ...animals,
+  ...bodyParts,
   ...foods
 ]
