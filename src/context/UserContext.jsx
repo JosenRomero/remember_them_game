df904ad0b9fd9bbaad6d052e7b5845c3850db077
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-17 16:28:36 -0600

    refactor UserContext file

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, createContext } from 'react';
-import { INITIAL_LEVEL, INITIAL_NUMBER_OF_WORDS, TOTAL_LEVELS } from '../utils/CONSTANTS'
+import { TOTAL_LEVELS } from '../utils/CONSTANTS'
 import { checkingCurrentData, setStorageItem } from '../utils/utils'
 
 export const UserContext = createContext()
@@ -11,37 +11,29 @@ export const UserProvider = ({ children }) => {
   const [ numberOfWords, setNumberOfWords] = useState(null);
 
   useEffect(() => {
+    let gameState = checkingCurrentData();
+    updateState(gameState);
+  }, [])
 
-    let { currentLevel, currentNumberOfWords, isLastLevelCompleted } = checkingCurrentData();
-    
+  const updateState = ({ currentLevel, currentNumberOfWords, isLastLevelCompleted }) => {
     setLevel(currentLevel);
     setNumberOfWords(currentNumberOfWords);
     setLastLevelCompleted(isLastLevelCompleted);
-
-  }, [])
+  }
 
   const nextLevel = () => {
 
     let gameState = { currentLevel: level, currentNumberOfWords: numberOfWords, isLastLevelCompleted: lastLevelCompleted }
 
     if(level < TOTAL_LEVELS) {
-      
       let newValue = level + 1;
       gameState.currentLevel = newValue;
-      setLevel(newValue);
-
-      if(newValue % 3 === 0 && newValue !== 9 && newValue !== 15) {
-        gameState.currentNumberOfWords = numberOfWords + 1;
-        setNumberOfWords(numberOfWords + 1);
-      }
-
+      if(newValue % 3 === 0 && newValue !== 9 && newValue !== 15) gameState.currentNumberOfWords = numberOfWords + 1;
     }
     
-    if(level === TOTAL_LEVELS) {
-      gameState.isLastLevelCompleted = true;
-      setLastLevelCompleted(true);
-    }
+    if(level === TOTAL_LEVELS) gameState.isLastLevelCompleted = true;
 
+    updateState(gameState);
     setStorageItem("gameState", gameState);
 
   }
