dab3b36934e745ea634bf6f9c469cff8ab9f1e5c
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-17 17:53:26 -0600

    style ResponseList file

--- src/components/ResponseList.jsx
+++ src/components/ResponseList.jsx
@@ -8,15 +8,17 @@ const ResponseList = ({ arr, responseId }) => {
 
   return (
     <div className="mt-6">
-      <div className="mb-4">{ t('text.addedAnswers') }</div>
-      {arr.map((response, i) => {
-        return (
-          <div className="flex mb-2" key={i}>
-            <p className="text-start">{ t('text.answer') } {i+1}: {response}</p>
-            <button className="bg-slate-500 hover:bg-slate-700 px-3 ml-3 rounded-xl font-semibold text-white" onClick={() => responseId(i)}>{ t('buttons.edit') }</button>
-          </div>
-        );
-      })}
+      <p className="mb-4">{ t('text.addedAnswers') }</p>
+      <div className="flex flex-col gap-5">
+        {arr.map((response, i) => {
+          return (
+            <div className="flex gap-3 items-center" key={i}>
+              <p className="text-start">{ t('text.answer') } {i+1}: {response}</p>
+              <button className="bg-slate-500 hover:bg-slate-700 px-3 py-1 rounded-xl font-semibold text-white" onClick={() => responseId(i)}>{ t('buttons.edit') }</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 
