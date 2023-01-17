c6a4bd95fc483c340b06cb0398e2cbc4359cf75d
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-17 17:48:19 -0600

    style Dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -14,7 +14,7 @@ const Dropdown = ({ changeLanguage, language }) => {
   return (
     <>
       <img src={language_translate} onClick={handlerDropdown} ref={ref} className="w-8 h-8 cursor-pointer hover:opacity-50" />
-      <div className={`${dropdown ? 'block' : 'hidden'} bg-white border rounded border-gray-400 absolute mt-2 py-2 min-w-[5rem]`}>
+      <div className={`${dropdown ? 'block' : 'hidden'} bg-white border rounded border-gray-400 absolute mt-10 py-2 min-w-[5rem]`}>
         <button onClick={changeLanguage} className={`w-full py-2 px-4 block ${language === 'es' ? 'bg-gray-300' : ''} hover:bg-gray-400`} value='es'>Español</button>
         <button onClick={changeLanguage} className={`w-full py-2 px-4 block ${language === 'en' ? 'bg-gray-300' : ''} hover:bg-gray-400`} value='en'>English</button>
       </div>
