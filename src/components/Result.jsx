ab78ff60c60efaf85af04234cc9af28d69c8c38a
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-19 18:52:57 -0600

    refactor ResultWordsList component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -9,7 +9,7 @@ const Result = ({ win, words, incorrectWord, result, lastLevelCompleted, level }
   return (
     <>
       <h2 className="text-center mt-5 mb-8 text-3xl">{ t('text.level') } {win ? level-1 : level}. { win ? t('result.complete') : t('result.failed') }</h2>
-      <ResultWordsList incorrectWord={incorrectWord} words={words} />
+      <ResultWordsList incorrectWord={incorrectWord} words={words} title={ t('result.title') } />
       { lastLevelCompleted && (
         <h3 className="bg-lime-700 text-white py-2 my-5 text-center">{ t('text.endText') }</h3>
       )}
--- src/components/ResultWordsList.jsx
+++ src/components/ResultWordsList.jsx
@@ -1,19 +1,26 @@
 
-const ResultWordsList = ({ incorrectWord, words}) => {
+const ResultWordsList = ({ incorrectWord, words, title }) => {
 
   return (
-    <div className="text-center">
-      {incorrectWord.map((word, i) => {
-        return (
-          <p 
-            key={i} 
-            className={`font-semibold mb-2 ${word === "" ? "text-lime-700" : "text-red-700 line-through"} `}
-          >
-            {word === "" ? words[i] : word}
-          </p>  
-        );
-      })}
-    </div>
+    <table className="table-auto mx-auto w-[90%] md:w-[50%]">
+      <thead>
+        <tr className="border border-slate-300">
+          <th className="py-2 px-3">{ title }</th>
+        </tr>
+      </thead>
+      <tbody>
+        {incorrectWord.map((word, i) => {
+          return (
+            <tr 
+              key={i} 
+              className={`font-semibold border border-slate-300 ${word === "" ? "text-lime-700" : "text-red-700 line-through"} `}
+            >
+              <td className="py-2 px-3 break-all">{word === "" ? words[i] : word}</td>
+            </tr>  
+          );
+        })}
+      </tbody>
+    </table>
   )
 
 }
