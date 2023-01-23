48ee670890dd89c8d517f61ee2579ad6d2b853c5
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-23 17:11:33 -0600

    style: changed color body

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,9 @@ import Footer from './Footer'
 const Layout = ({children}) => {
 
   return (
-    <div className="min-h-screen grid grid-cols-1 place-content-between bg-gray-100">
+    <div className="min-h-screen grid grid-cols-1 place-content-between bg-white">
       <Menu />
-      <main className="w-3/4 mx-auto min-h-[70vh] bg-white rounded-xl">
+      <main className="w-3/4 mx-auto min-h-[70vh] rounded-xl">
         <div className="h-full p-8 text-zinc-800">
           {children}
         </div>
