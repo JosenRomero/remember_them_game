e2dce90cc2a7fd638cf6d94bcc16bc3e57f6009c
Josen Romero <romerojosemanuel@hotmail.com>
2023-01-30 20:06:46 -0600

    add anime data in spanish

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,10 @@
-import { basic_data_in_spanish } from './dataInSpanish/index.js'
+import { basic_data_in_spanish, anime_data_in_spanish } from './dataInSpanish/index.js'
 import { basic_data_in_english, anime_data_in_english } from './dataInEnglish/index.js'
 
 export const words = {
   "es": {
     "basicMode": basic_data_in_spanish,
-    "animeMode": anime_data_in_english // TODO: spanish version 
+    "animeMode": anime_data_in_spanish
   },
   "en": {
     "basicMode": basic_data_in_english,
--- src/data/dataInSpanish/index.js
+++ src/data/dataInSpanish/index.js
@@ -1,9 +1,12 @@
 import { animalsInSpanish } from './basic/animalsInSpanish.js'
 import { bodyPartsInSpanish } from './basic/bodyPartsInSpanish.js'
 import { foodsInSpanish } from './basic/foodsInSpanish.js'
+import { animeInSpanish } from './anime/animeInSpanish.js'
 
 export const basic_data_in_spanish = [
   ...animalsInSpanish,
   ...bodyPartsInSpanish,
   ...foodsInSpanish
 ]
+
+export const anime_data_in_spanish = [...animeInSpanish]
